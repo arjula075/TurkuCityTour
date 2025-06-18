e76e9bebec7a99dc9316afd13a2a6a85f22ad6c5
arjula075 <ari.iska.lahti@gmail.com>
2025-06-18 20:49:16 +0300

    increased font size, more! even in map

--- src/pages/MapView.jsx
+++ src/pages/MapView.jsx
@@ -19,18 +19,18 @@ export default function MapView() {
 
     const handleLogout = async () => {
         await supabase.auth.signOut();
-        navigate('/login');
+        navigate('/');
     };
 
     return (
         <div className="flex flex-col items-center p-4 min-h-screen bg-white">
-            <h2 className="text-xl font-semibold mb-4 text-center">
+            <h2 className="text-5xl font-semibold mb-4 text-center">
                 Welcome, {profile?.first_name ?? user?.email ?? 'Guest'}
             </h2>
 
             {location ? (
                 <iframe
-                    className="w-full max-w-md h-64 rounded-md shadow-md"
+                    className="w-full h-64 rounded-md shadow-md"
                     loading="lazy"
                     allowFullScreen
                     src={`https://www.google.com/maps?q=${location.lat},${location.lng}&z=15&output=embed`}
