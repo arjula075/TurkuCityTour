f81b92aa478ad0aec583f027f5b8a1a58f2f19b9
arjula075 <ari.iska.lahti@gmail.com>
2025-06-18 20:43:17 +0300

    increased font size, more!

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -29,8 +29,8 @@ export default function Login() {
     };
 
     return (
-        <div className="p-4 max-w-md mx-auto">
-            <h1 className="text-3xl mb-4">Login</h1>
+        <div className="p-4 mx-auto">
+            <h1 className="text-6xl mb-4">Login</h1>
 
             <form onSubmit={handleLogin} className="flex flex-col gap-4">
                 <input
@@ -58,9 +58,9 @@ export default function Login() {
                 </button>
             </form>
 
-            <p className="mt-4 text-2xl  text-center">
+            <p className="mt-4 text-4xl  text-center">
                 Don't have an account?{' '}
-                <Link to="/register" className="text-blue-600 hover:underline text-3xl">
+                <Link to="/register" className="text-blue-600 hover:underline text-4xl">
                     Register here
                 </Link>
             </p>
