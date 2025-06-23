ee70373373017b4055a5254f30d1a8a864c418e6
arjula075 <ari.iska.lahti@gmail.com>
2025-06-23 11:41:12 +0300

    used a new map type, fixed a few bugs

--- src/pages/MapView.jsx
+++ src/pages/MapView.jsx
@@ -3,6 +3,22 @@ import { useAuthContext } from '../contexts/AuthContext';
 import { useNavigate } from 'react-router-dom';
 import { MapContainer, TileLayer, Marker, useMapEvents } from 'react-leaflet';
 
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+
+// Fix leaflet's default icon paths
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: markerIcon2x,
+    iconUrl: markerIcon,
+    shadowUrl: markerShadow,
+});
+
 export default function MapView() {
     const { user, profile, supabase } = useAuthContext();
     const [location, setLocation] = useState(null);
